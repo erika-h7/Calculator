6ec234661f5434bc06b5ad1b8f5283ad8daa1ace
erika-h7 <erika.herrera1@outlook.com>
2022-10-29 13:39:15 -0700

    Added functionality to the AC, +/-, % buttons.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,14 +12,33 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    var isFinishedTypingNumber : Bool = true
+    private var isFinishedTypingNumber : Bool = true
     
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
-        //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text in to a Double. ")
+        }
+        
+        if let calcMethod = sender.currentTitle {
+            
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+                
+            } else if calcMethod == "%" {
+                displayLabel.text = String(number / 100)
+                
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            }
+            
+            
+            
+        }
     }
 
     
